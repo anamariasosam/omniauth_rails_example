3a442690a92ac43c8aa6b801c97436e511bbb37d
Ana Maria Sosa <anamariasosam@gmail.com>
2017-06-08 19:40:27 -0500

    add home page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,7 @@
 class PagesController < ApplicationController
   def omniauth_login
   end
+  
+  def home
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   get 'pages/omniauth_login'
+  root 'pages#home'
   devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
 end
